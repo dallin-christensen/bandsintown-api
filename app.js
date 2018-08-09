864e46c8ec62e4ab441f8bd84f8fb8cefc4ab6ca
Dallin Christensen <dallin@pyxiscoin.com>
2018-08-09 10:26:19 -0700

    built interface for users

--- app.js
+++ app.js
@@ -3,7 +3,47 @@ const fs = require('file-system')
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const placeIntoScript = (js) => `<script>${js}</script>`
+const placeIntoScript = (js) => `
+<html>
+  <head>
+    <style>
+      body {
+        padding: 40px 0;
+        background-color: #4bf;
+        color: #fff;
+        font-family: sans-serif;
+        text-align: center;
+      }
+      #BandImg {
+        border-radius: 10px;
+      }
+      #Title {
+        font-size: 100px;
+      }
+      #Input {
+        border: 1px solid white;
+        border-radius: 3px;
+        padding: 3px;
+        margin-right: 10px;
+      }
+      #Button {
+        border: 1px solid white;
+        border-radius: 3px;
+        padding: 3px 10px;
+      }
+      #Button:active {
+        background-color: red;
+        color: white;
+      }
+    </style>
+  </head>
+  <body>
+  </body>
+  <script>
+    ${js}
+  </script>
+</html>
+`
 
 fs.readFile('index.js', 'utf8', (err, js) => {
   if (err) return
--- index.js
+++ index.js
@@ -1,13 +1,45 @@
-
 const apiKey = '66f140466808db7c1056daa086115759'
+
+const createPage = () => {
+  const container = document.createElement('div')
+  const title = document.createElement('h1')
+  const imgContainer = document.createElement('div')
+  const input = document.createElement('input')
+  const button = document.createElement('input')
+
+  input.type = 'text'
+  input.id = 'Input'
+  button.type = 'button'
+  button.value = 'search!'
+  button.id = 'Button'
+  button.onclick = () => {
+    const inputVal = document.getElementById('Input').value
+    getArtistData(inputVal)
+  }
+  title.id = 'Title'
+  imgContainer.id = 'BandImg'
+
+  container.appendChild(input)
+  container.appendChild(button)
+  container.appendChild(title)
+  container.appendChild(imgContainer)
+  document.body.appendChild(container)
+}
 
 const convertToBody = (data) => {
   const a = document.createElement('a')
   const img = document.createElement('img')
+  const title = document.createTextNode(data.name)
+  const domTitle = document.getElementById('Title')
+  const domImg = document.getElementById('BandImg')
+
   a.href = data.facebook_page_url
   img.src = data.image_url
   a.appendChild(img)
-  document.body.append(a)
+  domTitle.innerHTML = ''
+  domTitle.appendChild(title)
+  domImg.innerHTML = ''
+  domImg.appendChild(a)
 }
 
 const getArtistData = (artistName) => {
@@ -16,4 +48,4 @@ const getArtistData = (artistName) => {
     .then((data) => convertToBody(data))
 }
 
-getArtistData('afi')
+createPage()
